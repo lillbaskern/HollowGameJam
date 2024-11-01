ed54d6423b56b6b4c9856a39ebfbe8f022bb6980
lillbaskern <114910143+lillbaskern@users.noreply.github.com>
2024-11-01 20:08:16 +0100

    Imported TMPro essentials

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     void Start()
     {
         
+    }
+
+    public void OnStartGameClicked()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OnStartMultiplayerGameClicked()
+    {
+
     }
 
 }
