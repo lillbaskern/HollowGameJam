0dc7612b11c70aaa0ada2b6dd23679ebf565500e
lillbaskern <114910143+lillbaskern@users.noreply.github.com>
2024-11-01 20:14:27 +0100

    Placeholder main menu done, made cardmanager

---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GhostMessage.cs
@@ -0,0 +1,11 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GhostMessage : ScriptableObject
+{
+    public Sprite Sprite;
+
+    public Color Tint;
+
+    public string Message;
+}
